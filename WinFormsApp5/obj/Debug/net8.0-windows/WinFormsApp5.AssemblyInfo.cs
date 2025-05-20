825abec1e6cb8657a1ecd3b7705036efefd61d46
Artem4587 <artemneznayou@gmail.com>
2025-05-20 17:42:18 +0700

    ertah

--- WinFormsApp5/obj/Debug/net8.0-windows/WinFormsApp5.AssemblyInfo.cs
+++ WinFormsApp5/obj/Debug/net8.0-windows/WinFormsApp5.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp4")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b685428d44fce680c042765669a22225ee4674")]
-[assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp4")]
-[assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp4")]
+[assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp5")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
